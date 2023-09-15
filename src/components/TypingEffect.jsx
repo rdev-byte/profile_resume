41bf893ee1b4cfcda072f4e8ac5d3f3d4cd3e788
RDevByte <rdev.byte@gmail.com>
2023-09-14 22:06:21 -0600

    Incorporated Typing Effect

---
+++ src/components/TypingEffect.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Typed from 'react-typed';
+
+function TypingEffect() {
+    return (
+        <div>
+            <p>
+                I'm a{' '}
+                <Typed
+                    strings={[
+                        'Family Man',
+                        'Mentor',
+                        'Developer',
+                        'Researcher',
+                        'Tech Enthusiast',
+                        'Problem Solver',
+                        'Continuous Learner',
+                    ]}
+                    typeSpeed={50}
+                    backSpeed={25}
+                    backDelay={1000}
+                    loop
+                />
+            </p>
+        </div>
+    );
+}
+
+export default TypingEffect;
