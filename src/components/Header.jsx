d8a6df100abde12e5f47587501e9d361f1254518
RDevByte <rdev.byte@gmail.com>
2023-09-16 19:36:53 -0600

    deleted ContactModal.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link as ScrollLink } from 'react-scroll';
+import { Modal, Button } from 'react-bootstrap';
 
 function Header() {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleClose = () => setShowModal(false);
+    const handleShow = () => setShowModal(true);
+
     return (
         <header className="jumbotron">
             <div className="container">
                 <nav>
                     <ul className="nav justify-content-between">
                         <li className="nav-item">
-                            <a className="nav-link" href="#home">Home</a>
+                            <ScrollLink
+                                to="home"
+                                spy={true}
+                                smooth={true}
+                                duration={500}
+                            >
+                                Home
+                            </ScrollLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#about">About Me</a>
+                            <ScrollLink
+                                to="about"
+                                spy={true}
+                                smooth={true}
+                                duration={500}
+                            >
+                                About Me
+                            </ScrollLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#portfolio">Projects</a>
+                            <ScrollLink
+                                to="portfolio"
+                                spy={true}
+                                smooth={true}
+                                duration={500}
+                            >
+                                Projects
+                            </ScrollLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#contact">Contact Me</a>
+                            <a className="nav-link" href="#" onClick={handleShow}>
+                                Contact
+                            </a>
                         </li>
                     </ul>
                 </nav>
                 <p className="lead">I'm a passionate developer with a love for web development and programming languages.</p>
             </div>
-        </header>
+
+            {/* Modal */}
+            <Modal show={showModal} onHide={handleClose} centered>
+                <Modal.Dialog className="custom-modal">
+                    <Modal.Content>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Contact Me</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <p>LinkedIn</p>
+                            <p>GitHub</p>
+                            <p>Email</p>
+                            <p>Phone</p>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Close
+                            </Button>
+                        </Modal.Footer>
+                    </Modal.Content>
+                </Modal.Dialog>
+            </Modal>
+        </header >
     );
 }
 
